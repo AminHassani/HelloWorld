005fb42c6c85acde83d35cf44c52c93b9cb6a8af
AminHassani <61235868+AminHassani@users.noreply.github.com>
2020-02-26 16:18:24 +0100

    initial commit

---
+++ Source/MainComponent.cpp
@@ -0,0 +1,37 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+ /Users/ahassan/Users/ahassani/Desktop/Programming/Helloworld/Source/MainComponent.hi/Desktop/Programming/Helloworld/Source/MainComponent.h ==============================================================================
+*/
+
+#include "MainComponent.h"
+
+//==============================================================================
+MainComponent::MainComponent()
+{
+    setSize (600, 400);
+}
+
+MainComponent::~MainComponent()
+{
+}
+
+//==============================================================================
+void MainComponent::paint (Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+
+    g.setFont (Font (16.0f));
+    g.setColour (Colours::white);
+    g.drawText ("Hello World!", getLocalBounds(), Justification::centred, true);
+}
+
+void MainComponent::resized()
+{
+    // This is called when the MainComponent is resized.
+    // If you add any child components, this is where you should
+    // update their positions.
+}
